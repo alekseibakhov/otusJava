1902c1c64979c9ce0b34afcc3003fc6f4959c6b4
aleksei <bakhov93@inbox.ru>
2023-12-17 22:07:03 +0300

    исправил логику проверки наличия аннотации для метода

--- hw10-byteCodes/src/main/java/ru/otus/hw/DemoInvocationHandler.java
+++ hw10-byteCodes/src/main/java/ru/otus/hw/DemoInvocationHandler.java
@@ -7,19 +7,32 @@ import ru.otus.annotations.Log;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashSet;
 
 class DemoInvocationHandler implements InvocationHandler {
     private final TestLogging myClass;
+    private final HashSet<Method> methods;
     private static final Logger log = LoggerFactory.getLogger(DemoInvocationHandler.class);
 
     DemoInvocationHandler(TestLogging myClass) {
+        methods = new HashSet<>();
         this.myClass = myClass;
+        createAnnotationLogMethods(this.myClass.getClass().getDeclaredMethods());
+
+    }
+
+    private void createAnnotationLogMethods(Method[] methodList) {
+        for (Method method : methodList) {
+            if (method.isAnnotationPresent(Log.class)) {
+                methods.add(method);
+            }
+        }
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         method = TestLogging.class.getMethod("calculation", method.getParameterTypes());
-        if (method.isAnnotationPresent(Log.class)) {
+        if (methods.contains(method)) {
             log.info("Executed method: {}, param: {}", method.getName(), Arrays.toString(args));
         }
         return method.invoke(myClass, args);
